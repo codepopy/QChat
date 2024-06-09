2ba5d5bb2957066e6ed966eb7832a5193b3a746d
debojyoti <arya.bhattacharya.4622@gmail.com>
2024-06-09 13:02:40 +0530

    auth implementation

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Button from "@/components/ui/Button";
+import { db } from "@/lib/db";
+
+
+export default async function Home() {
+  return (
+    <>
+      <Button isLoading={false}>test</Button>
+    </>
+  );
+}
